1280e9893093c6b0419a504f8c7566867cd6b977
mpfdev <matheus.filgueiras17@gmail.com>
2022-11-05 09:51:51 -0300

    refactor: part1 exercise 1.3 - 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,18 +1,24 @@
-const Header = (props) => <h1>{props.course}</h1>;
+const Header = (props) => {
+  console.log(props.course.name);
+  return <h1>{props.course.name}</h1>;
+};
 
 const Content = (props) => {
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
+  console.log(props.parts);
   return (
     <>
-      <Part part={part1} exercise={exercises1} />
-      <Part part={part2} exercise={exercises2} />
-      <Part part={part3} exercise={exercises3} />
+      <Part
+        part={props.parts.parts[0].name}
+        exercise={props.parts.parts[0].exercises}
+      />
+      <Part
+        part={props.parts.parts[1].name}
+        exercise={props.parts.parts[1].exercises}
+      />
+      <Part
+        part={props.parts.parts[2].name}
+        exercise={props.parts.parts[2].exercises}
+      />
     </>
   );
 };
@@ -23,17 +29,40 @@ const Part = (props) => (
   </p>
 );
 
-const Footer = (props) => <p>Number of exercises {props.total}</p>;
+const Footer = (props) => {
+  //todo
+  const total =
+    props.parts.parts[0].exercises +
+    props.parts.parts[1].exercises +
+    props.parts.parts[2].exercises;
+
+  return <p>Number of exercises {total}</p>;
+};
 
 const App = () => {
-  const course = "Half Stack application development";
-  const totalExercises = 31;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Footer total={totalExercises} />
+      <Content parts={course} />
+      <Footer parts={course} />
     </div>
   );
 };
